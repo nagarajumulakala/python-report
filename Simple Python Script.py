c06e65130e1dfdf88dfb49b3dd9915c71be97e43
nagarajumulakala <nagaraju.mulakala2@gmail.com>
2026-05-05 11:29:12 +0530

    Code added 01

---
+++ Simple Python Script.py
@@ -0,0 +1,7 @@
+# This is a simple Python script that prints "Hello, World!" 
+# and calculates the sum of two numbers, A and B, then prints the result.
+print("Hello, World!")
+A = 5
+B = 10
+C = A + B
+print("The sum of A and B is:", C) 
